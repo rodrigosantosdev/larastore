9cabcce1a7b4d64ae8506f3f177833fce0e630a1
Yuri Neves <yurineves92@gmail.com>
2023-09-19 17:40:35 -0300

    correcao

--- resources/views/components/simple-advertise.blade.php
+++ resources/views/components/simple-advertise.blade.php
@@ -1,9 +1,8 @@
-<a href="{{ $href }}" class="bg-gray-100 p-4">
-    <img src="{{ $bgImage }}" alt="" class="w-full bg-cover h-[320px]">
-    <h1 class="text-2xl text-purple-800 mt-4">{{ $nameProduct }}</h1>
-    <p class="py-4">{{ $description }}</p>
-    <div class="flex justify-between items-center">
-        <span>Valor R$ {{ $price }}</span>
-        <button type="button" class="bg-purple-500 text-white p-2">Add Cart</button>
-    </div>
+<a href="{{ $href  }}" class="ad-item">
+    <div
+            class="ad-image"
+            style="background-image: url('{{ $bgImage }}')"
+    ></div>
+    <div class="ad-title">{{ $title  }}</div>
+    <div class="ad-price">{{ $price }}</div>
 </a>
